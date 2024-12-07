c1afc6246f24f01989ead0fca47444531cc409b1
“Ayush17416” <aagarwala073@gmail.com>
2024-12-07 16:36:48 +0530

    commited

---
+++ src/main/java/com/jdbc/Dao/JDBCConnector.java
@@ -0,0 +1,21 @@
+package com.jdbc.connector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCConnector {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/your_database";
+    private static final String USER = "your_username";
+    private static final String PASSWORD = "your_password";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error connecting to the database", e);
+        }
+    }
+}
---
+++ src/main/java/com/jdbc/connector/DonorController.java
@@ -0,0 +1,4 @@
+package com.jdbc.connector;
+
+public class DonorController {
+}
